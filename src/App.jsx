7252837454bdbd4e943c48aae9ebf686b790faf7
masterha <masterha@sheridancollege.ca>
2024-03-12 18:32:24 -0400

    Header, Footer, Note

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import styles from "./styles/styles.css";
+import Note from "./components/Note";
+function App() {
+  return (
+    <>
+      <Header />
+      <Note />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
